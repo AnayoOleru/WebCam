64c4f692e2ec3092f9375b4852a3dfbaa963109c
Anayo Oleru <anayo_oleru@outlook.com>
2019-08-08 07:02:10 +0100

    updates

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ function getVideo() {
   navigator.mediaDevices.getUserMedia({ video: true, audio: false })
     .then(localMediaStream => {
       console.log(localMediaStream);
+    
+//  DEPRECIATION : 
+//       The following has been depreceated by major browsers as of Chrome and Firefox.
+//       video.src = window.URL.createObjectURL(localMediaStream);
+//       Please refer to these:
+//       Depreceated  - https://developer.mozilla.org/en-US/docs/Web/API/URL/createObjectURL
+//       Newer Syntax - https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/srcObject
       
       video.srcObject = localMediaStream;
       video.play();
@@ -25,25 +32,31 @@ function paintToCanvas() {
 
   return setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height);
-
+    // take the pixels out
     let pixels = ctx.getImageData(0, 0, width, height);
+    // mess with them
+    // pixels = redEffect(pixels);
 
-    pixels = rgbSplit(pixels);
+    // pixels = rgbSplit(pixels);
+    // ctx.globalAlpha = 0.8;
 
+    // pixels = greenScreen(pixels);
+    // put them back
     ctx.putImageData(pixels, 0, 0);
   }, 16);
 }
 
 function takePhoto() {
-
+  // played the sound
   snap.currentTime = 0;
   snap.play();
 
+  // take the data out of the canvas
   const data = canvas.toDataURL('image/jpeg');
   const link = document.createElement('a');
   link.href = data;
   link.setAttribute('download', 'handsome');
-  link.innerHTML = `<img src="${data}" alt="webCamPhoto" />`;
+  link.innerHTML = `<img src="${data}" alt="WebCamPhoto" />`;
   strip.insertBefore(link, strip.firstChild);
 }
 
